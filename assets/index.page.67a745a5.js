c9335fc24bb587fbd99f0e51607889d3761e9d6a
Zack Cheng <zack_cheng@brown.edu>
2022-07-09 09:39:48 -0400

    Updated build | Jul 09, 09:39am

---
+++ assets/index.page.67a745a5.js
@@ -0,0 +1,8 @@
+import{r as c,j as e}from"./renderer/_default.page.client.jsx.ec3771fa.js";function r(){const[s,t]=c.exports.useState(0);return e.exports.jsxs("button",{type:"button",onClick:()=>t(o=>o+1),children:["Counter ",s]})}const l={title:"Home",hideTOC:!0};function n(s){const t=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",span:"span",p:"p",ul:"ul"},s.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(t.nav,{className:"toc",children:e.exports.jsx(t.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(t.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(t.a,{className:"toc-link toc-link-h1",href:"#welcome-to-csci-1230",children:"Welcome to CSCI 1230!"})})})}),e.exports.jsxs(t.main,{children:[`
+`,`
+`,e.exports.jsxs(t.h1,{id:"welcome-to-csci-1230",children:[e.exports.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#welcome-to-csci-1230",children:e.exports.jsx(t.span,{className:"icon icon-link"})}),"Welcome to CSCI 1230!"]}),`
+`,e.exports.jsx(t.p,{children:"This page is:"}),`
+`,e.exports.jsxs(t.ul,{children:[`
+`,e.exports.jsx(t.li,{children:"Pre-rendered to HTML."}),`
+`,e.exports.jsxs(t.li,{children:["Interactive. ",e.exports.jsx(r,{})]}),`
+`]})]})]})}function a(s={}){const{wrapper:t}=s.components||{};return t?e.exports.jsx(t,Object.assign({},s,{children:e.exports.jsx(n,s)})):n(s)}export{a as default,l as documentProps};
