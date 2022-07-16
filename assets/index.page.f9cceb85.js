0cafa9caa1615f3987e445da92bb895653071c27
Zack Cheng <zack_cheng@brown.edu>
2022-07-16 12:10:49 -0400

    Updated build | Jul 16, 12:10pm

--- assets/index.page.f9cceb85.js
+++ assets/index.page.f9cceb85.js
@@ -1,4 +1,4 @@
-import{j as s}from"./renderer/_default.page.client.jsx.6b7c91a2.js";const t={title:"Labs",hideTOC:!0};function r(n){const e=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",span:"span",p:"p"},n.components);return s.exports.jsxs(s.exports.Fragment,{children:[s.exports.jsx(e.nav,{className:"toc",children:s.exports.jsx(e.ol,{className:"toc-level toc-level-1",children:s.exports.jsx(e.li,{className:"toc-item toc-item-h1",children:s.exports.jsx(e.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),s.exports.jsxs(e.main,{children:[`
+import{j as s}from"./renderer/_default.page.client.jsx.f4e4c4d1.js";const t={title:"Labs",hideTOC:!0};function r(n){const e=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",span:"span",p:"p"},n.components);return s.exports.jsxs(s.exports.Fragment,{children:[s.exports.jsx(e.nav,{className:"toc",children:s.exports.jsx(e.ol,{className:"toc-level toc-level-1",children:s.exports.jsx(e.li,{className:"toc-item toc-item-h1",children:s.exports.jsx(e.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),s.exports.jsxs(e.main,{children:[`
 `,s.exports.jsxs(e.h1,{id:"labs",children:[s.exports.jsx(e.a,{"aria-hidden":"true",tabIndex:"-1",href:"#labs",children:s.exports.jsx(e.span,{className:"icon icon-link"})}),"Labs"]}),`
 `,s.exports.jsx(e.p,{children:"CS123 has 11 labs."}),`
 `,s.exports.jsx(e.p,{children:"Each lab is designed to take 30 - 60 min."}),`
@@ -10,7 +10,7 @@ import{j as s}from"./renderer/_default.page.client.jsx.6b7c91a2.js";const t={tit
 `,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab4",children:"Transforms"})}),`
 `,s.exports.jsx(e.li,{children:"Parsing"}),`
 `,s.exports.jsx(e.li,{children:"Light"}),`
-`,s.exports.jsx(e.li,{children:"Terrain"}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab7",children:"Terrain"})}),`
 `,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab8",children:"Trimeshes"})}),`
 `,s.exports.jsx(e.li,{children:"VBOs and VAOs"}),`
 `,s.exports.jsx(e.li,{children:"Shaders"}),`
