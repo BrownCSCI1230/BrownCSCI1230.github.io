0bf57284c562ad90f3deef5d55ba2a981e96a15f
Zack Cheng <zack_cheng@brown.edu>
2022-07-17 01:07:07 -0400

    Updated build | Jul 17, 01:06am

---
+++ assets/index.page.bc441af1.js
@@ -0,0 +1,23 @@
+import{j as e}from"./renderer/_default.page.client.jsx.a22a6670.js";const o={title:"Labs",hideTOC:!0};function t(r){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul"},r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#projects",children:"Projects"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"projects",children:e.exports.jsx(s.a,{href:"#projects",children:"Projects"})}),`
+`,e.exports.jsx(s.p,{children:"CS123 has 6 projects. Something something something."}),`
+`,e.exports.jsxs(s.ol,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/projects/raster",children:"Overview: Raster"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Raster1"}),`
+`,e.exports.jsx(s.li,{children:"Raster"}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/projects/ray",children:"Overview: Ray"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Ray1"}),`
+`,e.exports.jsx(s.li,{children:"Ray"}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.a,{href:"/projects/realtime",children:"Overview: Realtime"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Realtime1"}),`
+`,e.exports.jsx(s.li,{children:"Realtime2"}),`
+`]}),`
+`]}),`
+`]})]})]})}function i(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(t,r)})):t(r)}export{i as default,o as documentProps};
