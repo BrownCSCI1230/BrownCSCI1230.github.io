163ce4e244248075582dfda8def435a5f8e00a53
Zack Cheng <zack_cheng@brown.edu>
2022-07-18 02:44:29 -0400

    Updated build | Jul 18, 02:44am

---
+++ assets/index.page.1bb25be9.js
@@ -0,0 +1,18 @@
+import{j as s}from"./renderer/_default.page.client.jsx.8ee40795.js";const t={title:"Labs",hideTOC:!0};function r(n){const e=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},n.components);return s.exports.jsxs(s.exports.Fragment,{children:[s.exports.jsx(e.nav,{className:"toc",children:s.exports.jsx(e.ol,{className:"toc-level toc-level-1",children:s.exports.jsx(e.li,{className:"toc-item toc-item-h1",children:s.exports.jsx(e.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),s.exports.jsxs(e.main,{children:[`
+`,s.exports.jsx(e.h1,{id:"labs",children:s.exports.jsx(e.a,{href:"#labs",children:"Labs"})}),`
+`,s.exports.jsx(e.p,{children:"CS 1230 has 11 labs."}),`
+`,s.exports.jsx(e.p,{children:"Each lab is designed to take 30 - 60 min."}),`
+`,s.exports.jsx(e.p,{children:s.exports.jsx(e.a,{href:"/labs/lab0",children:"Dummy lab"})}),`
+`,s.exports.jsxs(e.ol,{children:[`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab1",children:"Setup and C++"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab2",children:"Pencils"})}),`
+`,s.exports.jsx(e.li,{children:"Convolution"}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab4",children:"Transforms"})}),`
+`,s.exports.jsx(e.li,{children:"Parsing"}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab6",children:"Light"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab7",children:"Terrain"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab8",children:"Trimeshes"})}),`
+`,s.exports.jsx(e.li,{children:"VBOs and VAOs"}),`
+`,s.exports.jsx(e.li,{children:"Shaders"}),`
+`,s.exports.jsx(e.li,{children:"Textures and FBOs"}),`
+`]})]})]})}function i(n={}){const{wrapper:e}=n.components||{};return e?s.exports.jsx(e,Object.assign({},n,{children:s.exports.jsx(r,n)})):r(n)}export{i as default,t as documentProps};
---
+++ assets/index.page.6e5d38a4.js
@@ -0,0 +1,29 @@
+import{r as t,j as e}from"./renderer/_default.page.client.jsx.8ee40795.js";function c(){const[n,r]=t.exports.useState(0);return e.exports.jsxs("button",{type:"button",onClick:()=>r(o=>o+1),children:["Counter ",n]})}const i={title:"Home"};function s(n){const r=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul"},n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(r.nav,{className:"toc",children:e.exports.jsxs(r.ol,{className:"toc-level toc-level-1",children:[e.exports.jsx(r.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(r.a,{className:"toc-link toc-link-h1",href:"#welcome-to-cs-1230",children:"Welcome to CS 1230!"})}),e.exports.jsx(r.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(r.a,{className:"toc-link toc-link-h1",href:"#calendar",children:"Calendar"})}),e.exports.jsx(r.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(r.a,{className:"toc-link toc-link-h1",href:"#staff",children:"Staff"})})]})}),e.exports.jsxs(r.main,{children:[`
+`,`
+`,e.exports.jsx(r.h1,{id:"welcome-to-cs-1230",children:e.exports.jsx(r.a,{href:"#welcome-to-cs-1230",children:"Welcome to CS 1230!"})}),`
+`,e.exports.jsx(r.p,{children:"This page is:"}),`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:"Pre-rendered to HTML."}),`
+`,e.exports.jsxs(r.li,{children:["Interactive. ",e.exports.jsx(c,{})]}),`
+`]}),`
+`,e.exports.jsx(r.h1,{id:"calendar",children:e.exports.jsx(r.a,{href:"#calendar",children:"Calendar"})}),`
+`,e.exports.jsx("iframe",{src:"https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showTz=0&showCalendars=1&showTitle=0&showTabs=1&showPrint=0&src=Y19oamgxc2hsMm5ybGZuZjBrMmp2NzZoZGRlc0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y18xaXZjYjI3cmpvcmtsbTFvMHFiaWdjZmpqc0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19uZnVtN3Q1YmIwaW1raTVmNDhycG1tZ3JkY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y19tcmpnNW1tazAyOGQ5ajRrYjA1bHV1dGF1Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000&color=%230B8043&color=%23EF6C00&color=%233F51B5",style:{borderRadius:10},width:"100%",height:"500",frameBorder:"0",scrolling:"no"}),`
+`,e.exports.jsx(r.h1,{id:"staff",children:e.exports.jsx(r.a,{href:"#staff",children:"Staff"})}),`
+`,e.exports.jsx(r.p,{children:"CS 1230 was built by the following lovely people!"}),`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"adrian_chang@brown.edu",children:"Adrian"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"angela_xing@brown.edu",children:"Angela"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"anna_j_zhao@brown.edu",children:"Anna"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"derick_toth@brown.edu",children:"Derick"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"geoffrey_hazard@brown.edu",children:"Geoffrey"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"helen_huang@brown.edu",children:"Helen"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"jianxin_gu@brown.edu",children:"Jianxin"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"logan_dooley@brown.edu",children:"Logan"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"marc_mapeke@brown.edu",children:"Marc"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"mehek_jethani@brown.edu",children:"Mehek"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"yiwen_huang1@brown.edu",children:"Nick"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"xiao_zhan@brown.edu",children:"Sean"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"yiwen_chen@brown.edu",children:"Yiwen"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"zack_cheng@brown.edu",children:"Zack"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"ziang_liu@brown.edu",children:"Ziang"})}),`
+`]})]})]})}function x(n={}){const{wrapper:r}=n.components||{};return r?e.exports.jsx(r,Object.assign({},n,{children:e.exports.jsx(s,n)})):s(n)}export{x as default,i as documentProps};
