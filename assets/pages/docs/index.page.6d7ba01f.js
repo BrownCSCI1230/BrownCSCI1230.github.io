ba0d8abbea258e5425f155497fdbeed4250ede56
Zack Cheng <zack_cheng@brown.edu>
2022-07-21 02:13:52 -0400

    Updated build | Jul 21, 02:13am

---
+++ assets/pages/docs/index.page.6d7ba01f.js
@@ -0,0 +1,13 @@
+import{j as e}from"../../chunk-903e764a.js";const c={title:"Docs",hideTOC:!0};function n(t){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",h2:"h2",ul:"ul"},t.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsxs(s.li,{className:"toc-item toc-item-h1",children:[e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#docs",children:"Docs"}),e.exports.jsxs(s.ol,{className:"toc-level toc-level-2",children:[e.exports.jsx(s.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h2",href:"#students",children:"Students"})}),e.exports.jsx(s.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h2",href:"#staff",children:"Staff"})})]})]})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"docs",children:e.exports.jsx(s.a,{href:"#docs",children:"Docs"})}),`
+`,e.exports.jsx(s.p,{children:"These are documents for staff and students' reference."}),`
+`,e.exports.jsx(s.h2,{id:"students",children:e.exports.jsx(s.a,{href:"#students",children:"Students"})}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"GitHub Guide"}),`
+`]}),`
+`,e.exports.jsx(s.h2,{id:"staff",children:e.exports.jsx(s.a,{href:"#staff",children:"Staff"})}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/docs/code-blocks",children:"Code blocks"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/docs/inline-code",children:"Inline code"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/docs/cpp-guidelines",children:"C++ guidelines"})}),`
+`]})]})]})}function r(t={}){const{wrapper:s}=t.components||{};return s?e.exports.jsx(s,Object.assign({},t,{children:e.exports.jsx(n,t)})):n(t)}export{r as default,c as documentProps};
---
+++ assets/pages/labs/index.page.08f10f21.js
@@ -0,0 +1,18 @@
+import{j as s}from"../../chunk-903e764a.js";const t={title:"Labs",hideTOC:!0};function n(r){const e=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},r.components);return s.exports.jsxs(s.exports.Fragment,{children:[s.exports.jsx(e.nav,{className:"toc",children:s.exports.jsx(e.ol,{className:"toc-level toc-level-1",children:s.exports.jsx(e.li,{className:"toc-item toc-item-h1",children:s.exports.jsx(e.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),s.exports.jsxs(e.main,{children:[`
+`,s.exports.jsx(e.h1,{id:"labs",children:s.exports.jsx(e.a,{href:"#labs",children:"Labs"})}),`
+`,s.exports.jsx(e.p,{children:"CS 1230 has 11 labs."}),`
+`,s.exports.jsx(e.p,{children:"Each lab is designed to take 30 - 60 min."}),`
+`,s.exports.jsx(e.p,{children:s.exports.jsx(e.a,{href:"/labs/lab0",children:"Dummy lab"})}),`
+`,s.exports.jsxs(e.ol,{children:[`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab1",children:"Setup and C++"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab2",children:"Pencils"})}),`
+`,s.exports.jsx(e.li,{children:"Convolution"}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab4",children:"Transforms"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab5",children:"Parsing"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab6",children:"Light"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab7",children:"Terrain"})}),`
+`,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab8",children:"Trimeshes"})}),`
+`,s.exports.jsx(e.li,{children:"VBOs and VAOs"}),`
+`,s.exports.jsx(e.li,{children:"Shaders"}),`
+`,s.exports.jsx(e.li,{children:"Textures and FBOs"}),`
+`]})]})]})}function i(r={}){const{wrapper:e}=r.components||{};return e?s.exports.jsx(e,Object.assign({},r,{children:s.exports.jsx(n,r)})):n(r)}export{i as default,t as documentProps};
