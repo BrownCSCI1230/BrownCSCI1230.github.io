449ed84ad7b9feb35088996738df00b95447fa5d
Zack Cheng <zack_cheng@brown.edu>
2022-07-25 03:14:22 -0400

    Updated build | Jul 25, 03:14am

--- assets/pages/labs/index.page.0666e65a.js
+++ assets/pages/labs/index.page.0666e65a.js
@@ -2,7 +2,7 @@ import{j as s}from"../../chunk-903e764a.js";const t={title:"Labs",hideTOC:!0};fu
 `,s.exports.jsx(e.h1,{id:"labs",children:s.exports.jsx(e.a,{href:"#labs",children:"Labs"})}),`
 `,s.exports.jsx(e.p,{children:"CS 1230 has 11 labs."}),`
 `,s.exports.jsx(e.p,{children:"Each lab is designed to take 30 - 60 min."}),`
-`,s.exports.jsx(e.p,{children:s.exports.jsx(e.a,{href:"/labs/lab0",children:"Dummy lab"})}),`
+`,s.exports.jsx(e.p,{children:s.exports.jsx(e.a,{href:"/labs/dummy",children:"Dummy lab"})}),`
 `,s.exports.jsxs(e.ol,{children:[`
 `,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab1",children:"Setup and C++"})}),`
 `,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab2",children:"Pencils"})}),`
@@ -15,4 +15,4 @@ import{j as s}from"../../chunk-903e764a.js";const t={title:"Labs",hideTOC:!0};fu
 `,s.exports.jsx(e.li,{children:"VBOs and VAOs"}),`
 `,s.exports.jsx(e.li,{children:s.exports.jsx(e.a,{href:"/labs/lab10",children:"Shaders"})}),`
 `,s.exports.jsx(e.li,{children:"Textures and FBOs"}),`
-`]})]})]})}function a(r={}){const{wrapper:e}=r.components||{};return e?s.exports.jsx(e,Object.assign({},r,{children:s.exports.jsx(n,r)})):n(r)}export{a as default,t as documentProps};
+`]})]})]})}function x(r={}){const{wrapper:e}=r.components||{};return e?s.exports.jsx(e,Object.assign({},r,{children:s.exports.jsx(n,r)})):n(r)}export{x as default,t as documentProps};
