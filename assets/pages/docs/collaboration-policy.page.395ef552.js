813dae43f6105bdbc819aebf363d2a9c44aafc40
Zack Cheng <zack_cheng@brown.edu>
2022-08-09 12:06:02 -0400

    Updated build | Aug 09, 12:05pm

--- assets/pages/docs/collaboration-policy.page.395ef552.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"../../chunk-903e764a.js";import{M as s}from"../../chunk-b2e2a089.js";const c={title:"Collaboration Policy"};function n(t){const e=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},t.components);return o.exports.jsxs(o.exports.Fragment,{children:[o.exports.jsx(e.nav,{className:"toc",children:o.exports.jsx(e.ol,{className:"toc-level toc-level-1",children:o.exports.jsx(e.li,{className:"toc-item toc-item-h1",children:o.exports.jsx(e.a,{className:"toc-link toc-link-h1",href:"#collaboration-policy",children:"Collaboration Policy"})})})}),o.exports.jsxs(e.main,{children:[`
-`,`
-`,o.exports.jsx(e.h1,{id:"collaboration-policy",children:o.exports.jsx(e.a,{href:"#collaboration-policy",children:"Collaboration Policy"})}),`
-`,o.exports.jsx(e.p,{children:"Hello!"}),`
-`,o.exports.jsx(e.p,{children:"This page is under construction."}),`
-`,o.exports.jsx(s,{})]})]})}function i(t={}){const{wrapper:e}=t.components||{};return e?o.exports.jsx(e,Object.assign({},t,{children:o.exports.jsx(n,t)})):n(t)}export{i as default,c as documentProps};
