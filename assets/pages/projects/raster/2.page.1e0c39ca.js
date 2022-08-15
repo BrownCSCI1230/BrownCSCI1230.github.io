6a5ac5586072205b493d60e1f357b67793e9d089
Zack Cheng <zack_cheng@brown.edu>
2022-08-14 22:38:10 -0700

    Updated build | Aug 14, 10:37pm

--- assets/pages/projects/raster/2.page.1e0c39ca.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"../../../chunk-6ef03c1b.js";import{M as s}from"../../../chunk-b9ea0eaf.js";const i={title:"Project 2"};function r(o){const t=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",h2:"h2"},o.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(t.nav,{className:"toc",children:e.exports.jsx(t.ol,{className:"toc-level toc-level-1",children:e.exports.jsxs(t.li,{className:"toc-item toc-item-h1",children:[e.exports.jsx(t.a,{className:"toc-link toc-link-h1",href:"#project-2-filter",children:"Project 2: Filter"}),e.exports.jsx(t.ol,{className:"toc-level toc-level-2",children:e.exports.jsx(t.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(t.a,{className:"toc-link toc-link-h2",href:"#introduction",children:"Introduction"})})})]})})}),e.exports.jsxs(t.main,{children:[`
-`,`
-`,e.exports.jsx(t.h1,{id:"project-2-filter",children:e.exports.jsx(t.a,{href:"#project-2-filter",children:"Project 2: Filter"})}),`
-`,e.exports.jsxs(t.p,{children:["You can find the algo for this project ",e.exports.jsx(t.a,{href:"/projects/raster/2-algo",children:"here"}),"."]}),`
-`,e.exports.jsx(t.h2,{id:"introduction",children:e.exports.jsx(t.a,{href:"#introduction",children:"Introduction"})}),`
-`,e.exports.jsx(t.p,{children:"Hello!"}),`
-`,e.exports.jsx(t.p,{children:"This page is under construction."}),`
-`,e.exports.jsx(s,{})]})]})}function l(o={}){const{wrapper:t}=o.components||{};return t?e.exports.jsx(t,Object.assign({},o,{children:e.exports.jsx(r,o)})):r(o)}export{l as default,i as documentProps};
