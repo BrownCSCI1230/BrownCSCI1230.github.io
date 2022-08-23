fd4a5c399d72dd25910d172f9d6c970a0e187184
axing2 <angela_xing@brown.edu>
2022-08-23 17:38:21 -0400

    Updated build | Aug 23, 05:37PM

--- assets/pages/projects/raster/1-algo.page.a8fbc0cd.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"../../../chunk-83577a7c.js";import{M as r}from"../../../chunk-da990835.js";const l={title:"Project 1 (Algo)"};function s(t){const o=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},t.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(o.nav,{className:"toc",children:e.exports.jsx(o.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(o.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h1",href:"#project-1-brush-algo",children:"Project 1: Brush (Algo)"})})})}),e.exports.jsxs(o.main,{children:[`
-`,`
-`,e.exports.jsx(o.h1,{id:"project-1-brush-algo",children:e.exports.jsx(o.a,{href:"#project-1-brush-algo",children:"Project 1: Brush (Algo)"})}),`
-`,e.exports.jsxs(o.p,{children:["You can find the handout for this project ",e.exports.jsx(o.a,{href:"/projects/raster/1",children:"here"}),"."]}),`
-`,e.exports.jsx(o.p,{children:"Hello!"}),`
-`,e.exports.jsx(o.p,{children:"This page is under construction."}),`
-`,e.exports.jsx(r,{})]})]})}function a(t={}){const{wrapper:o}=t.components||{};return o?e.exports.jsx(o,Object.assign({},t,{children:e.exports.jsx(s,t)})):s(t)}export{a as default,l as documentProps};
--- assets/pages/projects/ray/1-algo.page.5285c87f.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"../../../chunk-83577a7c.js";import{M as n}from"../../../chunk-da990835.js";const i={title:"Project 3 (Algo)"};function s(o){const t=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},o.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(t.nav,{className:"toc",children:e.exports.jsx(t.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(t.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(t.a,{className:"toc-link toc-link-h1",href:"#project-3-intersect-algo",children:"Project 3: Intersect (Algo)"})})})}),e.exports.jsxs(t.main,{children:[`
-`,`
-`,e.exports.jsx(t.h1,{id:"project-3-intersect-algo",children:e.exports.jsx(t.a,{href:"#project-3-intersect-algo",children:"Project 3: Intersect (Algo)"})}),`
-`,e.exports.jsxs(t.p,{children:["You can find the handout for this project ",e.exports.jsx(t.a,{href:"/projects/ray/1",children:"here"}),"."]}),`
-`,e.exports.jsx(t.p,{children:"Hello!"}),`
-`,e.exports.jsx(t.p,{children:"This page is under construction."}),`
-`,e.exports.jsx(n,{})]})]})}function l(o={}){const{wrapper:t}=o.components||{};return t?e.exports.jsx(t,Object.assign({},o,{children:e.exports.jsx(s,o)})):s(o)}export{l as default,i as documentProps};
