3c7a70d4cd8256bb79557ae31cfc82e89ce69f1d
Zack Cheng <zack_cheng@brown.edu>
2022-09-29 12:37:01 -0400

    Updated build | Sep 29, 12:36pm

---
+++ assets/pages/projects/index.page.ea4470cd.js
@@ -0,0 +1,28 @@
+import{j as e}from"../../chunk-37395b21.js";const o={title:"Projects",hideTOC:!0};function n(r){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul",strong:"strong"},r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#projects",children:"Projects"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"projects",children:e.exports.jsx(s.a,{href:"#projects",children:"Projects"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 6 projects + 1 final project. Each spans 2 weeks."}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"2D"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 1: ",e.exports.jsx(s.a,{href:"/projects/2D/1",children:"Brush"})," (",e.exports.jsx(s.a,{href:"/projects/2D/1-algo-ans",children:"Algo Answers"}),")"]}),`
+`,e.exports.jsxs(s.li,{children:["Project 2: ",e.exports.jsx(s.a,{href:"/projects/2D/2",children:"Filter"})," (",e.exports.jsx(s.a,{href:"/projects/2D/2-algo-ans",children:"Algo Answers"}),")"]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Ray"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Project 3: Intersect (Algo)"}),`
+`,e.exports.jsx(s.li,{children:"Project 4: Illuminate (Algo)"}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Realtime"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Project 5: Lights, Camera (Algo)"}),`
+`,e.exports.jsx(s.li,{children:"Project 6: Action! (Algo)"}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Final"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Project 7: Final Project"}),`
+`]}),`
+`]}),`
+`]})]})]})}function l(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(n,r)})):n(r)}export{l as default,o as documentProps};
