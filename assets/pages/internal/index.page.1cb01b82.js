2ae3fe4dde12fefed739074ef976d4e5cd7eda68
Dylan Hu <git@dylan.hu>
2023-09-04 15:41:19 -0400

    Updated build | Sep 04, 03:41PM

--- assets/pages/internal/index.page.1cb01b82.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"../../chunk-b0d73203.js";const o={title:"Internal",hideTOC:!0};function s(t){const n=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",strong:"strong",ul:"ul"},t.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(n.nav,{className:"toc",children:e.exports.jsx(n.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(n.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(n.a,{className:"toc-link toc-link-h1",href:"#internal",children:"Internal"})})})}),e.exports.jsxs(n.main,{children:[`
-`,e.exports.jsx(n.h1,{id:"internal",children:e.exports.jsx(n.a,{href:"#internal",children:"Internal"})}),`
-`,e.exports.jsxs(n.p,{children:["These are documents for TAs' reference only. ",e.exports.jsx(n.strong,{children:"Do not share these documents with students."})]}),`
-`,e.exports.jsxs(n.ul,{children:[`
-`,e.exports.jsx(n.li,{children:e.exports.jsx(n.a,{href:"/internal/mentor-meetings-guide",children:"Mentor Meetings Guide"})}),`
-`]})]})]})}function l(t={}){const{wrapper:n}=t.components||{};return n?e.exports.jsx(n,Object.assign({},t,{children:e.exports.jsx(s,t)})):s(t)}export{l as default,o as documentProps};
---
+++ assets/pages/projects/index.page.971e0bfa.js
@@ -0,0 +1,38 @@
+import{j as e}from"../../chunk-137b9351.js";const o={title:"Projects",hideTOC:!0};function r(n){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul",strong:"strong"},n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#projects",children:"Projects"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"projects",children:e.exports.jsx(s.a,{href:"#projects",children:"Projects"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 6 projects + 1 final project. Each spans 2 weeks."}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Raster"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 1: ",e.exports.jsx(s.a,{href:"/projects/raster/1",children:"Brush"})]}),`
+`,e.exports.jsxs(s.li,{children:["Project 2: Filter",`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Ray"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Project 3: Intersect"}),`
+`,e.exports.jsxs(s.li,{children:["Project 4: Illuminate",`
+`,`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Realtime"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:"Project 5: Lights, Camera"}),`
+`,e.exports.jsxs(s.li,{children:["Project 6: Action!",`
+`,`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Final"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsxs(s.li,{children:["Project 7: Final Project",`
+`,`
+`]}),`
+`]}),`
+`]}),`
+`]})]})]})}function c(n={}){const{wrapper:s}=n.components||{};return s?e.exports.jsx(s,Object.assign({},n,{children:e.exports.jsx(r,n)})):r(n)}export{c as default,o as documentProps};
