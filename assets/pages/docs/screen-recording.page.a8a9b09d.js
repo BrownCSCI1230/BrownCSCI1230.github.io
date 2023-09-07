494c71282b2b42cbe79a38f60586244ba8a670c2
obloomfield <obloomfield@brown.edu>
2023-09-06 22:58:59 -0400

    Updated build | Sep 06, 10:58PM

---
+++ assets/pages/docs/screen-recording.page.a8a9b09d.js
@@ -0,0 +1,20 @@
+import{j as e}from"../../chunk-91776ecf.js";import{M as r}from"../../chunk-cab91a23.js";const c={title:"Screen Recording Guide"};function n(s){const o=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",h2:"h2",code:"code",blockquote:"blockquote"},s.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(o.nav,{className:"toc",children:e.exports.jsx(o.ol,{className:"toc-level toc-level-1",children:e.exports.jsxs(o.li,{className:"toc-item toc-item-h1",children:[e.exports.jsx(o.a,{className:"toc-link toc-link-h1",href:"#screen-recording-guide",children:"Screen Recording Guide"}),e.exports.jsxs(o.ol,{className:"toc-level toc-level-2",children:[e.exports.jsx(o.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#mac-os",children:"Mac OS"})}),e.exports.jsx(o.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#windows",children:"Windows"})}),e.exports.jsx(o.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#linux-department-machines",children:"Linux (Department Machines)"})}),e.exports.jsx(o.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#linux-other-distros",children:"Linux (Other Distros)"})})]})]})})}),e.exports.jsxs(o.main,{children:[`
+`,`
+`,e.exports.jsx(o.h1,{id:"screen-recording-guide",children:e.exports.jsx(o.a,{href:"#screen-recording-guide",children:"Screen Recording Guide"})}),`
+`,e.exports.jsx(o.p,{children:`Some of the assignments in this course require you to submit screen recordings of your code in action.
+This guide will walk you through how to do that for different operating systems.
+Note that this guide is meant to be suggestive, not prescriptive: if you have some other way that you already know about and like for making screen recordings, feel free to use that.`}),`
+`,e.exports.jsx(o.h2,{id:"mac-os",children:e.exports.jsx(o.a,{href:"#mac-os",children:"Mac OS"})}),`
+`,e.exports.jsxs(o.p,{children:["Mac OS has built-in screen recording functionality. You can bring up the screen recording interface by pressing ",e.exports.jsx(o.code,{children:"Command + Shift + 5"}),` on your keyboard.
+`,e.exports.jsx(o.a,{href:"https://support.apple.com/en-us/HT208721",target:"_blank",rel:"noopener noreferrer",children:"This page"})," has a good overview of how to use the screen recording interface."]}),`
+`,e.exports.jsx(o.h2,{id:"windows",children:e.exports.jsx(o.a,{href:"#windows",children:"Windows"})}),`
+`,e.exports.jsxs(o.blockquote,{children:[`
+`,e.exports.jsx(o.p,{children:"TODO"}),`
+`]}),`
+`,e.exports.jsx(o.h2,{id:"linux-department-machines",children:e.exports.jsx(o.a,{href:"#linux-department-machines",children:"Linux (Department Machines)"})}),`
+`,e.exports.jsxs(o.blockquote,{children:[`
+`,e.exports.jsx(o.p,{children:"TODO"}),`
+`]}),`
+`,e.exports.jsx(o.h2,{id:"linux-other-distros",children:e.exports.jsx(o.a,{href:"#linux-other-distros",children:"Linux (Other Distros)"})}),`
+`,e.exports.jsx(o.p,{children:"If you run Linux on your own personal machine, we're going to assume that you're capable of figuring out on your own how to do screen recordings ;)"}),`
+`,e.exports.jsx(r,{})]})]})}function a(s={}){const{wrapper:o}=s.components||{};return o?e.exports.jsx(o,Object.assign({},s,{children:e.exports.jsx(n,s)})):n(s)}export{a as default,c as documentProps};
