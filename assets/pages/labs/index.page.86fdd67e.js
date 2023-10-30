0c0d05b2c70d8037d73c49ce1437be8f10c9c297
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2023-10-30 18:49:29 -0400

    Updated build | Oct 30, 06:49PM

---
+++ assets/pages/labs/index.page.86fdd67e.js
@@ -0,0 +1,16 @@
+import{j as e}from"../../chunk-b0d73203.js";const t={title:"Labs",hideTOC:!0};function n(r){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},r.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"labs",children:e.exports.jsx(s.a,{href:"#labs",children:"Labs"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 11 labs. Each is designed to take 1-2 h."}),`
+`,e.exports.jsxs(s.ol,{children:[`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab1",children:"Setup & C++"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab2",children:"Pencils"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab3",children:"Convolution"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab4",children:"Transforms"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab5",children:"Scene Parsing"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab6",children:"Light"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab7",children:"Terrain"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab8",children:"Trimeshes"})}),`
+`,e.exports.jsx(s.li,{children:"VBOs & VAOs"}),`
+`,e.exports.jsx(s.li,{children:"Shaders"}),`
+`,e.exports.jsx(s.li,{children:"Textures & FBOs"}),`
+`]})]})]})}function i(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(n,r)})):n(r)}export{i as default,t as documentProps};
