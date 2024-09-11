1d6eb4bc29244859a79982381f792f44490413ac
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2024-09-11 17:09:58 -0400

    Updated build | Sep 11, 05:09PM

---
+++ assets/pages/projects/index.page.11ab24e7.js
@@ -0,0 +1,26 @@
+import{j as e}from"../../chunk-b0d73203.js";const o={title:"Projects",hideTOC:!0};function t(s){const n=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul",strong:"strong"},s.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(n.nav,{className:"toc",children:e.exports.jsx(n.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(n.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(n.a,{className:"toc-link toc-link-h1",href:"#projects",children:"Projects"})})})}),e.exports.jsxs(n.main,{children:[`
+`,e.exports.jsx(n.h1,{id:"projects",children:e.exports.jsx(n.a,{href:"#projects",children:"Projects"})}),`
+`,e.exports.jsx(n.p,{children:"CS 1230 has 6 projects + 1 final project. Each spans 2 weeks."}),`
+`,e.exports.jsxs(n.ul,{children:[`
+`,e.exports.jsxs(n.li,{children:[e.exports.jsx(n.strong,{children:"Raster"}),`
+`,e.exports.jsxs(n.ul,{children:[`
+`,e.exports.jsxs(n.li,{children:[e.exports.jsx(n.a,{href:"/projects/raster/1",children:"Project 1: Brush"}),`
+`,`
+`]}),`
+`]}),`
+`,`
+`,`
+`]}),`
+`]}),`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,`
+`,`
+`]})]})}function c(s={}){const{wrapper:n}=s.components||{};return n?e.exports.jsx(n,Object.assign({},s,{children:e.exports.jsx(t,s)})):t(s)}export{c as default,o as documentProps};
