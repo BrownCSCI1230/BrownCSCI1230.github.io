30abc61a8ac08b2fc3a5ef3e3084d2caa9645568
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2024-10-09 21:16:55 -0400

    Updated build | Oct 09, 09:16PM

---
+++ assets/pages/labs/index.page.a0ce6cdd.js
@@ -0,0 +1,16 @@
+import{j as e}from"../../chunk-b0d73203.js";const l={title:"Labs",hideTOC:!0};function r(n){const s=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p"},n.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(s.nav,{className:"toc",children:e.exports.jsx(s.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(s.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(s.a,{className:"toc-link toc-link-h1",href:"#labs",children:"Labs"})})})}),e.exports.jsxs(s.main,{children:[`
+`,e.exports.jsx(s.h1,{id:"labs",children:e.exports.jsx(s.a,{href:"#labs",children:"Labs"})}),`
+`,e.exports.jsx(s.p,{children:"CS 1230 has 11 labs. Each is designed to take 1-2 h."}),`
+`,e.exports.jsxs(s.ol,{children:[`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab1",children:"Setup & C++"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab2",children:"Pencils"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab3",children:"Convolution"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab4",children:"Transforms"})}),`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/labs/lab5",children:"Scene Parsing"})}),`
+`]}),`
+`,`
+`,`
+`,`
+`,`
+`,`
+`]})]})}function o(n={}){const{wrapper:s}=n.components||{};return s?e.exports.jsx(s,Object.assign({},n,{children:e.exports.jsx(r,n)})):r(n)}export{o as default,l as documentProps};
