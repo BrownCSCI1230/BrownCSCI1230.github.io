21cda4b7dc84ea91a2c20e5c17cc76a35671b030
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2024-10-15 12:23:13 -0400

    Updated build | Oct 15, 12:22PM

---
+++ assets/pages/docs/mentor-meetings-guide.page.36139e37.js
@@ -0,0 +1,50 @@
+import{j as e}from"../../chunk-b0d73203.js";import{M as s}from"../../chunk-82f9daf4.js";const c={title:"Mentor Meetings Guide"};function r(t){const o=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",h2:"h2",p:"p",h3:"h3",strong:"strong",code:"code"},t.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(o.nav,{className:"toc",children:e.exports.jsx(o.ol,{className:"toc-level toc-level-1",children:e.exports.jsxs(o.li,{className:"toc-item toc-item-h1",children:[e.exports.jsx(o.a,{className:"toc-link toc-link-h1",href:"#mentor-meetings-guide",children:"Mentor Meetings Guide"}),e.exports.jsxs(o.ol,{className:"toc-level toc-level-2",children:[e.exports.jsx(o.li,{className:"toc-item toc-item-h2",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#overview",children:"Overview"})}),e.exports.jsxs(o.li,{className:"toc-item toc-item-h2",children:[e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#before-your-meetings",children:"Before your meetings"}),e.exports.jsxs(o.ol,{className:"toc-level toc-level-3",children:[e.exports.jsx(o.li,{className:"toc-item toc-item-h3",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h3",href:"#preparing",children:"Preparing"})}),e.exports.jsx(o.li,{className:"toc-item toc-item-h3",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h3",href:"#rescheduling",children:"Rescheduling"})})]})]}),e.exports.jsxs(o.li,{className:"toc-item toc-item-h2",children:[e.exports.jsx(o.a,{className:"toc-link toc-link-h2",href:"#during-your-meetings",children:"During your meetings"}),e.exports.jsx(o.ol,{className:"toc-level toc-level-3",children:e.exports.jsx(o.li,{className:"toc-item toc-item-h3",children:e.exports.jsx(o.a,{className:"toc-link toc-link-h3",href:"#opening-the-project",children:"Opening the project"})})})]})]})]})})}),e.exports.jsxs(o.main,{children:[`
+`,`
+`,e.exports.jsx(o.h1,{id:"mentor-meetings-guide",children:e.exports.jsx(o.a,{href:"#mentor-meetings-guide",children:"Mentor Meetings Guide"})}),`
+`,e.exports.jsx(o.h2,{id:"overview",children:e.exports.jsx(o.a,{href:"#overview",children:"Overview"})}),`
+`,e.exports.jsx(o.p,{children:"In CS 1230, mentor meetings serve a multitude of purposes. Among other things, they are a chance for you to:"}),`
+`,e.exports.jsxs(o.ol,{start:"2",children:[`
+`,e.exports.jsx(o.li,{children:"Review a project submission"}),`
+`,e.exports.jsx(o.li,{children:"Ask questions about a project"}),`
+`,e.exports.jsx(o.li,{children:"Ask questions about the course"}),`
+`,e.exports.jsx(o.li,{children:"Get a lab checked off"}),`
+`]}),`
+`,e.exports.jsx(o.p,{children:"Through meeting with you on a weekly basis, your mentor will develop a strong sense of your progress within the course and be able to provide you with personalized feedback and advice."}),`
+`,e.exports.jsx(o.p,{children:"This guide will walk you through the process of preparing for and attending your mentor meetings."}),`
+`,e.exports.jsx(o.h2,{id:"before-your-meetings",children:e.exports.jsx(o.a,{href:"#before-your-meetings",children:"Before your meetings"})}),`
+`,e.exports.jsx(o.p,{children:"Check your Google Calendar to see when your meeting is scheduled. It should be the same time and location every week, and your mentor will email you if they need to reschedule or relocate."}),`
+`,e.exports.jsx(o.p,{children:"You should always come to your meeting prepared with"}),`
+`,e.exports.jsxs(o.ol,{children:[`
+`,e.exports.jsx(o.li,{children:"Your computer (charged)"}),`
+`,e.exports.jsx(o.li,{children:"Questions about the course or current project"}),`
+`,e.exports.jsx(o.li,{children:"Any materials you need to get a lab checked off"}),`
+`]}),`
+`,e.exports.jsx(o.h3,{id:"preparing",children:e.exports.jsx(o.a,{href:"#preparing",children:"Preparing"})}),`
+`,e.exports.jsx(o.p,{children:"Prepare for your meeting by"}),`
+`,e.exports.jsxs(o.ol,{children:[`
+`,e.exports.jsx(o.li,{children:"Finding the commit id of your code submission"}),`
+`,e.exports.jsx(o.li,{children:"Knowing where on your computer the code submission is located"}),`
+`,e.exports.jsx(o.li,{children:"Writing down any questions you had about the code or project in general"}),`
+`]}),`
+`,e.exports.jsx(o.h3,{id:"rescheduling",children:e.exports.jsx(o.a,{href:"#rescheduling",children:"Rescheduling"})}),`
+`,e.exports.jsxs(o.p,{children:["If you need to reschedule, let your mentor know via email ",e.exports.jsx(o.strong,{children:"as soon as possible"}),". They will try to accommodate you, but they may not be able to find a time that works for both of you. In that case, you can work with your mentor to find a different mentor to meet with for the week."]}),`
+`,e.exports.jsx(o.h2,{id:"during-your-meetings",children:e.exports.jsx(o.a,{href:"#during-your-meetings",children:"During your meetings"})}),`
+`,e.exports.jsx(o.p,{children:"In order of priority, your mentor will"}),`
+`,e.exports.jsxs(o.ol,{children:[`
+`,e.exports.jsx(o.li,{children:"Review with you the code submission that you submitted the week before"}),`
+`,e.exports.jsx(o.li,{children:"Answer any questions you have about the course or current project"}),`
+`,e.exports.jsx(o.li,{children:"Check off any labs you need checked off"}),`
+`]}),`
+`,e.exports.jsx(o.p,{children:"Note that your mentor will not extend your meeting past the scheduled time. If you expect that time will not permit for your mentor to check off a lab, you should plan accordingly - we recommend getting labs checked off during TA hours."}),`
+`,e.exports.jsx(o.h3,{id:"opening-the-project",children:e.exports.jsx(o.a,{href:"#opening-the-project",children:"Opening the project"})}),`
+`,e.exports.jsx(o.p,{children:"For your mentor to be sure that the code you demonstrate is the code you submitted, you must check out the commit id of your code submission during your meeting."}),`
+`,e.exports.jsx(o.p,{children:"Remember that you may find your commit id in your Gradescope submission."}),`
+`,e.exports.jsxs(o.ol,{children:[`
+`,e.exports.jsx(o.li,{children:"Open a terminal and navigate to the project directory."}),`
+`,e.exports.jsxs(o.li,{children:["Run ",e.exports.jsx(o.code,{children:"git status"})," to make sure you are on the correct branch and have no uncommitted changes. If you have uncommitted changes, commit them or stash them with ",e.exports.jsx(o.code,{children:"git stash"}),". You can unstash them later with ",e.exports.jsx(o.code,{children:"git stash pop"}),"."]}),`
+`,e.exports.jsxs(o.li,{children:["Run ",e.exports.jsx(o.code,{children:"git checkout <commit id>"}),". This will put you in a detached head state, which is fine."]}),`
+`,e.exports.jsxs(o.li,{children:["Run ",e.exports.jsx(o.code,{children:"git status"})," again to make sure you are on the correct commit."]}),`
+`,e.exports.jsxs(o.li,{children:["Run ",e.exports.jsx(o.code,{children:"qtcreator ."})," to open the project in Qt Creator. If you do not have the ",e.exports.jsx(o.code,{children:"qtcreator"})," command installed, you can open the project by opening Qt Creator and selecting ",e.exports.jsx(o.code,{children:"File > Open File or Project..."})," and selecting the project directory."]}),`
+`]}),`
+`,e.exports.jsxs(o.p,{children:["After your meeting, you may want to return to the ",e.exports.jsx(o.code,{children:"main"})," branch. To do so, run ",e.exports.jsx(o.code,{children:"git checkout main"}),"."]}),`
+`,e.exports.jsx(s,{})]})]})}function l(t={}){const{wrapper:o}=t.components||{};return o?e.exports.jsx(o,Object.assign({},t,{children:e.exports.jsx(r,t)})):r(t)}export{l as default,c as documentProps};
