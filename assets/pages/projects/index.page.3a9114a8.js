8eefc92fc11a570ee9f871ee4cdc15e74cf07d5b
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2024-11-05 22:12:30 -0500

    Updated build | Nov 05, 10:12PM

--- assets/pages/projects/index.page.3a9114a8.js
+++ assets/pages/projects/index.page.3a9114a8.js
@@ -41,6 +41,9 @@ import{j as e}from"../../chunk-b0d73203.js";const o={title:"Projects",hideTOC:!0
 `,`
 `,`
 `]}),`
+`,e.exports.jsxs(s.li,{children:[e.exports.jsx(s.strong,{children:"Final"}),`
+`,e.exports.jsxs(s.ul,{children:[`
+`,e.exports.jsx(s.li,{children:e.exports.jsx(s.a,{href:"/projects/final",children:"Project 7: Final Project"})}),`
 `]}),`
-`,`
-`]})]})}function l(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(n,r)})):n(r)}export{l as default,o as documentProps};
+`]}),`
+`]})]})]})}function l(r={}){const{wrapper:s}=r.components||{};return s?e.exports.jsx(s,Object.assign({},r,{children:e.exports.jsx(n,r)})):n(r)}export{l as default,o as documentProps};
