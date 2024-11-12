61b9508561344e55631456a876a1112e9421020b
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2024-11-11 19:04:28 -0500

    Updated build | Nov 11, 07:04PM

---
+++ assets/pages/docs/index.page.2b8e2472.js
@@ -0,0 +1,43 @@
+import{j as e}from"../../chunk-b0d73203.js";const i={title:"Docs",hideTOC:!0};function n(s){const r=Object.assign({nav:"nav",ol:"ol",li:"li",a:"a",main:"main",h1:"h1",p:"p",ul:"ul",strong:"strong"},s.components);return e.exports.jsxs(e.exports.Fragment,{children:[e.exports.jsx(r.nav,{className:"toc",children:e.exports.jsx(r.ol,{className:"toc-level toc-level-1",children:e.exports.jsx(r.li,{className:"toc-item toc-item-h1",children:e.exports.jsx(r.a,{className:"toc-link toc-link-h1",href:"#docs",children:"Docs"})})})}),e.exports.jsxs(r.main,{children:[`
+`,e.exports.jsx(r.h1,{id:"docs",children:e.exports.jsx(r.a,{href:"#docs",children:"Docs"})}),`
+`,e.exports.jsx(r.p,{children:"These are documents for students' reference."}),`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsxs(r.li,{children:["Course",`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.strong,{children:e.exports.jsx(r.a,{href:"/docs/course-missive",children:"Course Missive"})})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/collaboration-policy",children:"Collaboration Policy"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/github-gradescope-guide",children:"GitHub + Gradescope Guide"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/mentor-meetings-guide",children:"Mentor Meetings Guide"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/screen-recording",children:"Screen Recording Guide"})}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(r.li,{children:["Scenefiles",`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"https://scenes.cs1230.graphics",target:"_blank",rel:"noopener noreferrer",children:"Scenes"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/scenes-tutorial",children:"Scenes Tutorial"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/scenefile-documentation",children:"Scenefile Documentation"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"https://github.com/BrownCSCI1230/scenefiles",target:"_blank",rel:"noopener noreferrer",children:"Scenefiles Repository"})}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(r.li,{children:["Coding",`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/advanced-cpp-tutorial",children:"Going Deeper with C++"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/code-readability-guidelines",children:"Code Readability Guidelines"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/preventing-memory-leaks",children:"Preventing Memory Leaks"})}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(r.li,{children:["Other Resources",`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/xcode-CLT-installation-guide",children:"Xcode Command Line Tools Installation Guide"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"/docs/demos",children:"Demos"})}),`
+`]}),`
+`]}),`
+`,e.exports.jsxs(r.li,{children:["Previous Offerings of This Course",`
+`,e.exports.jsxs(r.ul,{children:[`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"http://cs1230.graphics/website-fall-23/",target:"_blank",rel:"noopener noreferrer",children:"Fall 2023"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"http://cs1230.graphics/website-fall-22/",target:"_blank",rel:"noopener noreferrer",children:"Fall 2022"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"http://cs.brown.edu/courses/cs123/archive/2021/",target:"_blank",rel:"noopener noreferrer",children:"Fall 2021"})}),`
+`,e.exports.jsx(r.li,{children:e.exports.jsx(r.a,{href:"http://cs.brown.edu/courses/cs123/archive/2020/",target:"_blank",rel:"noopener noreferrer",children:"Fall 2020"})}),`
+`]}),`
+`]}),`
+`]})]})]})}function t(s={}){const{wrapper:r}=s.components||{};return r?e.exports.jsx(r,Object.assign({},s,{children:e.exports.jsx(n,s)})):n(s)}export{t as default,i as documentProps};
